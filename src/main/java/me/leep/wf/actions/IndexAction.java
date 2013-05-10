d59c49f9f6dd40c1359ec126ec7ba5c51b30f70d
qiuqiupeng <pengleeg@gmail.com>
2013-05-10 18:47:58 +0800

    提交indexActin类

---
+++ src/main/java/me/leep/wf/actions/IndexAction.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package me.leep.wf.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author lipeng
+ *
+ */
+public class IndexAction extends ActionSupport {
+
+	/* (non-Javadoc)
+	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 */
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+	
+
+}
