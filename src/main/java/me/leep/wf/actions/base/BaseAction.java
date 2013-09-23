c10d0eefd98a5207a38d8a92da51ccca6deb9f3f
qiuqiupeng <pengleeg@gmail.com>
2013-09-23 16:18:32 +0800

    添加action日志

--- src/main/java/me/leep/wf/actions/base/BaseAction.java
+++ src/main/java/me/leep/wf/actions/base/BaseAction.java
@@ -3,6 +3,9 @@
  */
 package me.leep.wf.actions.base;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -15,5 +18,15 @@ public class BaseAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	public static final Logger logger;
+	
+	static {
+		logger = Logger.getLogger("");
+		logger.setLevel(Level.ALL);
+	}
+	
+	public static void log(String info, Level level, Throwable ex) {
+    	logger.log(level, info, ex);
+    }
 
 }
--- src/main/java/me/leep/wf/actions/system/UserEditAction.java
+++ src/main/java/me/leep/wf/actions/system/UserEditAction.java
@@ -1,9 +1,9 @@
 package me.leep.wf.actions.system;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.logging.Level;
 
 import me.leep.wf.actions.base.EditAction;
 import me.leep.wf.dto.system.User;
@@ -13,38 +13,49 @@ import me.leep.wf.services.system.aware.IUserServices;
 import me.leep.wf.util.CodeUtil;
 import net.sf.cglib.beans.BeanCopier;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class UserEditAction extends EditAction {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4096802666866337184L;
 
 	private String rowid;
 	private User user;
 
+	/* 以下是图片属性 */
+	private static final int BUFFER_SIZE = 16 * 1024;
+	private File myFile;
+	private String contentType;
+	private String fileName;
+	private String imageFileName;
+	private String caption;
+	/* 以上是图片属性 */
+
 	private IUserServices userServices;
 
 	@Override
 	public String execute() throws Exception {
+		logger.log(Level.ALL, ">>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+		imageFileName = "img/profile.png";
 		user = userServices.initUserDto(rowid);
 		return SUCCESS;
 	}
 
 	@Override
 	public String save() throws Exception {
-		System.out.println(">>>>>>>>" + User.class.getName() + "save2>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>" + User.class.getName()
+				+ "save2>>>>>>>>>>>>>");
 		if (StringUtils.isNotEmpty(user.getNumber())
 				&& StringUtils.isNotEmpty(user.getName())
 				&& StringUtils.isNotEmpty(user.getPassword())
-				&& StringUtils.isNotEmpty(user.getRepassword())
-				) {
+				&& StringUtils.isNotEmpty(user.getRepassword())) {
 			if (StringUtils.equals(user.getPassword(), user.getRepassword())) {
-				UserBean bean = (UserBean) userServices.findById(user.getId(), UserBean.class);
-				if(bean == null) {
+				UserBean bean = (UserBean) userServices.findById(user.getId(),
+						UserBean.class);
+				if (bean == null) {
 					user.setPassword(CodeUtil.getStringMD5(user.getPassword()));
-					BeanCopier copy = BeanCopier.create(User.class, UserBean.class,
-							false);
+					BeanCopier copy = BeanCopier.create(User.class,
+							UserBean.class, false);
 					BaseEntiy entity = new UserBean();
 					copy.copy(user, entity, null);
 					userServices.save(entity);
@@ -61,19 +72,18 @@ public class UserEditAction extends EditAction {
 					messages.add("修改成功");
 					setActionMessages(messages);
 				}
-				
+
 			} else {
 				List<String> errors = new ArrayList<String>();
 				errors.add("输入的密码不一致！");
 				setActionErrors(errors);
 			}
-				
+
 		} else {
 			List<String> errors = new ArrayList<String>();
 			errors.add("数据为空");
 			setActionErrors(errors);
 		}
-			
 
 		return SUCCESS;
 	}
@@ -89,21 +99,23 @@ public class UserEditAction extends EditAction {
 	public String delete() throws Exception {
 		System.out.println("----------------delete--------------");
 		if (StringUtils.isBlank(rowid) && user != null) {
-			UserBean entity = (UserBean) userServices.findById(user.getId(), UserBean.class);
+			UserBean entity = (UserBean) userServices.findById(user.getId(),
+					UserBean.class);
 			userServices.delete(entity, UserBean.class);
 			user = new User();
 			return SUCCESS;
 		} else {
-			if(StringUtils.isNotBlank(rowid)) {
+			if (StringUtils.isNotBlank(rowid)) {
 				String[] ids = rowid.split(",");
-				for (int i = 0 ; i < ids.length; i++) {
-					UserBean entity = (UserBean) userServices.findById(ids[i], UserBean.class);
+				for (int i = 0; i < ids.length; i++) {
+					UserBean entity = (UserBean) userServices.findById(ids[i],
+							UserBean.class);
 					userServices.delete(entity, UserBean.class);
 				}
 			}
 			return "user-list";
 		}
-		
+
 	}
 
 	/**
@@ -149,6 +161,88 @@ public class UserEditAction extends EditAction {
 	 */
 	public void setRowid(String rowid) {
 		this.rowid = rowid;
+	}
+
+	/**
+	 * @return myFile
+	 */
+	public File getMyFile() {
+		return myFile;
+	}
+
+	/**
+	 * @param myFile
+	 *            要设置的 myFile
+	 */
+	public void setMyFile(File myFile) {
+		this.myFile = myFile;
+	}
+
+	/**
+	 * @return contentType
+	 */
+	public String getContentType() {
+		return contentType;
+	}
+
+	/**
+	 * @param contentType
+	 *            要设置的 contentType
+	 */
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	/**
+	 * @return fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName
+	 *            要设置的 fileName
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @return imageFileName
+	 */
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	/**
+	 * @param imageFileName
+	 *            要设置的 imageFileName
+	 */
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+	/**
+	 * @return caption
+	 */
+	public String getCaption() {
+		return caption;
+	}
+
+	/**
+	 * @param caption
+	 *            要设置的 caption
+	 */
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	/**
+	 * @return bufferSize
+	 */
+	public static int getBufferSize() {
+		return BUFFER_SIZE;
 	}
 
 }
