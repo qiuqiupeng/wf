f0fd4e8a4ef775c9412e7e7446be014ea531f4a4
qiuqiupeng <pengleeg@gmail.com>
2013-10-07 03:02:31 +0800

    整合调试activiti成功，另外调试使用spring-data成功

--- src/main/java/me/leep/wf/services/system/impl/UserServicesImpl.java
+++ src/main/java/me/leep/wf/services/system/impl/UserServicesImpl.java
@@ -5,14 +5,19 @@ package me.leep.wf.services.system.impl;
 
 import java.util.UUID;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.activiti.engine.IdentityService;
+import org.activiti.engine.ProcessEngine;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.leep.wf.dto.BaseDto;
 import me.leep.wf.dto.system.User;
+import me.leep.wf.entity.BaseEntiy;
 import me.leep.wf.entity.system.UserBean;
+import me.leep.wf.repository.system.UserRepository;
 import me.leep.wf.services.BaseServiceImpl;
 import me.leep.wf.services.system.aware.IUserServices;
 import me.leep.wf.util.BeanUtil;
@@ -23,9 +28,13 @@ import me.leep.wf.util.BeanUtil;
  */
 @Service("userServices")
 public class UserServicesImpl extends BaseServiceImpl implements IUserServices {
-	
+	@Autowired
+	private ProcessEngine processEngine;
 	@Autowired
 	private IdentityService identityService;
+
+	@Autowired
+	private UserRepository userRepository;// 注入UserRepository
 
 	@Override
 	public User initUserDto(String rowid) {
@@ -33,27 +42,64 @@ public class UserServicesImpl extends BaseServiceImpl implements IUserServices {
 		user.setId(UUID.randomUUID().toString());
 		if (StringUtils.isNotEmpty(rowid)) {
 			UserBean userBean = (UserBean) findById(rowid, UserBean.class);
-			if (userBean != null) 
-			BeanUtil.copyBean(userBean, user);
+			if (userBean != null)
+				BeanUtil.copyBean(userBean, user);
 		}
 		return user;
 	}
 
-	/* （非 Javadoc）
-	 * @see me.leep.wf.services.BaseServiceImpl#save(me.leep.wf.dto.BaseDto, java.lang.Class)
+	/*
+	 * （非 Javadoc）
+	 * 
+	 * @see me.leep.wf.services.BaseServiceImpl#save(me.leep.wf.dto.BaseDto,
+	 * java.lang.Class)
 	 */
 	@Override
 	public void save(BaseDto dto, Class clazz) {
-		// TODO 自动生成的方法存根
-		super.save(dto, clazz);
-		org.activiti.engine.identity.User activitiUser = identityService.newUser(dto.getNumber());
-		activitiUser.setFirstName(dto.getName());
-		identityService.saveUser(activitiUser);
+		// super.save(dto, clazz);
+
+		BaseEntiy bean;
+		if (dto.getId() != null)
+			bean = userRepository.findById(dto.getId());
+		else
+			bean = null;
+		if (bean != null) {
+			dto.setCreater(bean.getCreater());
+			dto.setCreteTime(bean.getCreteTime());
+		}
+		UserBean entity;
+		try {
+			entity = (UserBean) Class.forName(clazz.getName()).newInstance();
+			BeanUtil.copyBean(dto, entity);
+			entity.setId(UUID.randomUUID().toString());
+			userRepository.save(entity);
+			
+			identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+			
+		} catch (InstantiationException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+		
+		
+
 	}
 
-
 	/**
-	 * @param identityService 要设置的 identityService
+	 * @param identityService
+	 *            要设置的 identityService
 	 */
 	public void setIdentityService(IdentityService identityService) {
 		this.identityService = identityService;
--- src/test/java/me/leep/wf/test/services/ActivitiUserServiceTest.java
+++ src/test/java/me/leep/wf/test/services/ActivitiUserServiceTest.java
@@ -2,12 +2,19 @@ package me.leep.wf.test.services;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 import javax.transaction.TransactionManager;
 
 import me.leep.wf.entity.system.UserBean;
 import me.leep.wf.services.system.aware.IUserServices;
 
 import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.Picture;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.impl.util.IoUtil;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,42 +33,106 @@ public class ActivitiUserServiceTest {
 	@Autowired
 	private IdentityService identityService;
 	
-	@Autowired
-	private TransactionManager transactionManager;
 
 	@Test
 	public void testInsertUser() {
-	    try  
-	    {  
-	    	transactionManager.begin();//开启事务  
-	        identityService.saveUser(identityService.newUser("kermit"));  
-	        identityService.saveUser(identityService.newUser("gonzo"));    
-	        
-	        
-			org.activiti.engine.identity.User activitiUser = identityService.newUser("11111");
-			activitiUser.setFirstName("test");
-			identityService.saveUser(activitiUser);
+
+//	    	transactionManager.begin();//开启事务  
+//	        identityService.saveUser(identityService.newUser("kermit"));  
+//	        identityService.saveUser(identityService.newUser("gonzo"));    
+//	        
+//	        
+//			org.activiti.engine.identity.User activitiUser = identityService.newUser("11111");
+//			activitiUser.setFirstName("test");
+//			identityService.saveUser(activitiUser);
+//			
+//	        transactionManager.commit();//提交事务  
+	    	
+//			System.out.println(identityService.createUserQuery().userId("1").count());
 			
-	        transactionManager.commit();//提交事务  
+			initDemoUsers();
 	        
 
-			assertNotNull("Person object is null but should not be.", activitiUser);
-			
-			logger.info("Person state is " + activitiUser);
-	    }  
-	    catch(Throwable e)  
-	    {  
-	        try {  
-	        	transactionManager.rollback();//回滚事务  
-	        } catch (Exception e1) {  
-	            // TODO Auto-generated catch block  
-	            e1.printStackTrace();  
-	        }  
-	    } 
+//			assertNotNull("Person object is null but should not be.", activitiUser);
+//			
+//			logger.info("Person state is " + activitiUser);
+
 		
 
 		
 		
 	}
+	
+	protected void initDemoUsers() {
+//	    createUser("kermit", "Kermit", "The Frog", "kermit", "kermit@activiti.org", 
+//	            "org/activiti/explorer/images/kermit.jpg",
+//	            Arrays.asList("management", "sales", "marketing", "engineering", "user", "admin"),
+//	            Arrays.asList("birthDate", "10-10-1955", "jobTitle", "Muppet", "location", "Hollywoord",
+//	                          "phone", "+123456789", "twitterName", "alfresco", "skype", "activiti_kermit_frog"));
+//	    
+//	    createUser("gonzo", "Gonzo", "The Great", "gonzo", "gonzo@activiti.org", 
+//	            "org/activiti/explorer/images/gonzo.jpg",
+//	            Arrays.asList("management", "sales", "marketing", "user"),
+//	            null);
+//	    createUser("fozzie", "Fozzie", "Bear", "fozzie", "fozzie@activiti.org", 
+//	            "org/activiti/explorer/images/fozzie.jpg",
+//	            Arrays.asList("marketing", "engineering", "user"),
+//	            null);
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+		identityService.saveUser(identityService.newUser(UUID.randomUUID().toString()));
+	  }
 
+	
+	protected void createUser(String userId, String firstName, String lastName, String password, 
+	          String email, String imageResource, List<String> groups, List<String> userInfo) {
+	    
+	    if (identityService.createUserQuery().userId(userId).count() == 0) {
+	      
+	      // Following data can already be set by demo setup script
+	      
+	      User user = identityService.newUser(userId);
+	      user.setFirstName(firstName);
+	      user.setLastName(lastName);
+	      user.setPassword(password);
+	      user.setEmail(email);
+	      identityService.saveUser(user);
+	      
+	      if (groups != null) {
+	        for (String group : groups) {
+	          identityService.createMembership(userId, group);
+	        }
+	      }
+	    }
+	    
+	    // Following data is not set by demo setup script
+	      
+	    // image
+//	    if (imageResource != null) {
+//	      byte[] pictureBytes = IoUtil.readInputStream(this.getClass().getClassLoader().getResourceAsStream(imageResource), null);
+//	      Picture picture = new Picture(pictureBytes, "image/jpeg");
+//	      identityService.setUserPicture(userId, picture);
+//	    }
+	      
+	    // user info
+	    if (userInfo != null) {
+	      for(int i=0; i<userInfo.size(); i+=2) {
+	        identityService.setUserInfo(userId, userInfo.get(i), userInfo.get(i+1));
+	      }
+	    }
+	    
+	  }
 }
