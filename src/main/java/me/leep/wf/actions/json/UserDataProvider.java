b51965f09d4d521b6778990b30451cd9134ff8c6
qiuqiupeng <pengleeg@gmail.com>
2014-01-19 02:41:36 +0800

    Signed-off-by: qiuqiupeng <pengleeg@gmail.com>

--- src/main/java/me/leep/wf/actions/json/UserDataProvider.java
+++ src/main/java/me/leep/wf/actions/json/UserDataProvider.java
@@ -2,6 +2,8 @@ package me.leep.wf.actions.json;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import me.leep.wf.actions.base.BaseDataProvider;
 import me.leep.wf.dto.BaseDto;
 import me.leep.wf.services.system.aware.IUserServices;
@@ -17,15 +19,13 @@ public class UserDataProvider extends BaseDataProvider {
 	 */
 	private static final long serialVersionUID = 4302036849790278738L;
 
+	@Autowired
 	private IUserServices userServices;
 
 	public String execute() throws Exception {
 
-//		initGridParam(UserBean.class);
 		initGridParam(userServices.countAll());
 
-		// List<BaseDto> result = userServices.findAll(UserBean.class,
-		// User.class, null, getFrom(), getTo());
 		List<BaseDto> result = userServices.findAll(getPage().intValue() - 1,
 				getTo() - getFrom());
 
@@ -34,12 +34,6 @@ public class UserDataProvider extends BaseDataProvider {
 		return SUCCESS;
 	}
 
-	/**
-	 * @param userServices
-	 *            要设置的 userServices
-	 */
-	public void setUserServices(IUserServices userServices) {
-		this.userServices = userServices;
-	}
+
 
 }
