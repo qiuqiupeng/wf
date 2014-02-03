5c63d7c0e15b8eeabacd3a4bdcf4c844b557ccfc
qiuqiupeng <pengleeg@gmail.com>
2014-02-04 00:39:23 +0800

    调整格式
    Signed-off-by: qiuqiupeng <pengleeg@gmail.com>

--- src/main/java/me/leep/wf/repository/system/MessageRepository.java
+++ src/main/java/me/leep/wf/repository/system/MessageRepository.java
@@ -1,13 +1,10 @@
 package me.leep.wf.repository.system;
 
-
-
 import me.leep.wf.entity.system.MessageBean;
 import me.leep.wf.repository.BaseRepository;
 
-
 /**
- * 用户持久层接口
+ * 消息中心持久层接口
  * 
  * @author 李鹏 2012-8-31
  */
